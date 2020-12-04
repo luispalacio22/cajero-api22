de41bfc4b7810835aafe6b646ae743c91b7589de
Luis Palacio <luispalacio11c@gmail.com>
2020-12-03 19:10:49 -0500

    primer commit
    primer commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from db.user_db import UserInDB
+from db.user_db import update_user, get_user
+from db.transaction_db import TransactionInDB
+from db.transaction_db import save_transaction
+from models.user_models import UserIn, UserOut
+from models.transaction_models import TransactionIn,TransactionOut
+import datetime
+from fastapi import FastAPI
+from fastapi import HTTPException
+
+api = FastAPI()
+
+
+@api.post("/user/auth/")
+async def auth_user(user_in: UserIn):
+    user_in_db = get_user(user_in.username)
+    if user_in_db == None:
+        raise HTTPException(status_code=404,
+                detail="El usuario no existe")
+    if user_in_db.password != user_in.password:
+        raise HTTPException(status_code=403, detail="Error de autenticacion")
+          
+    return {"Autenticado": True}
+
+@api.get("/user/balance/{username}")
+async def get_balance(username: str):
+    user_in_db = get_user(username)
+    if user_in_db == None:
+        raise HTTPException(status_code=404,
+            detail="El usuario no existe")
+    user_out = UserOut(**user_in_db.dict())
+    return user_out
+
+@api.put("/user/transaction/")
+async def make_transaction(transaction_in: TransactionIn):
+    user_in_db = get_user(transaction_in.username)
+    if user_in_db == None:
+        raise HTTPException(status_code=404,
+            detail="El usuario no existe")
+    if user_in_db.balance < transaction_in.value:
+        raise HTTPException(status_code=400,
+            detail="Sin fondos suficientes")
