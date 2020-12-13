a64fb9dcc776f0219eb8c406355b660be4260bb1
Luis Palacio <luispalacio11c@gmail.com>
2020-12-13 02:51:19 -0500

    cambio

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ api = FastAPI()
 
 origins = [
     "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8080",
+    "http://localhost", "http://localhost:8080","https://cajero-app22.herokuapp.com/",
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
